a35f0a43962c2628df39983c06237c4a05de54f1
Suchit Vemula <vemulasuchit3@gmail.com>
2023-06-03 11:32:41 -0400

    Added user and admin models

---
+++ src/Model/Administator.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Blogpress\Model;
+
+class Administrator {
+    protected $user_id;
+    protected $salt;
+    protected $username;
+    protected $email;
+    protected $type = "administrator";
+
+
+    /**
+     * Get the value of user_id
+     */ 
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set the value of user_id
+     *
+     * @return  self
+     */ 
+    public function setUser_id($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of salt
+     */ 
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Set the value of salt
+     *
+     * @return  self
+     */ 
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     */ 
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @return  self
+     */ 
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of type
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */ 
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
